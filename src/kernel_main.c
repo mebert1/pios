6834b4a5bb6d51b0ac4e08aaef733cb9c1aa1809
Marius Ebert <mebert1@luc.edu>
2021-02-01 17:44:49 -0600

    add function to clear bss

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,19 @@
 
+extern int __bss_start;
+extern int __bss_end;
+extern int __bss_size;
 
+void clear_bss(void) {
 
-void kernel_main() {
+	char* begin_bss	= (char*) & __bss_start;
+	
+	for(int i = 0; i < __bss_size; i++) {
+		begin_bss[i] = 0;
+	}
+}
 
-    while(1){
-    }
+void kernel_main(void) {
+	
+	clear_bss();
+	while(1){}
 }
